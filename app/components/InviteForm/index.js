90d505ab37ab6c29653fc2457a8f6bd21fcc2ee0
Ural Davletshin <hunter1271@yandex.ru>
2018-01-26 00:49:46 +0300

    User list and invite form

--- app/components/InviteForm/index.js
+++ app/components/InviteForm/index.js
@@ -1,21 +1,46 @@
 import React from 'react';
-// import { object, node, func } from 'prop-types';
+import { object, func, bool } from 'prop-types';
+import { connect } from 'react-redux';
 import { compose, pure } from 'recompose';
 import { FormattedMessage } from 'react-intl';
 import Button from 'material-ui/Button';
-import TextField from 'material-ui/TextField';
+import { TextField } from 'redux-form-material-ui';
 import Dialog, {
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
 } from 'material-ui/Dialog';
+import { CircularProgress } from 'material-ui/Progress';
+import { isEmail, isRequired } from 'utils/validators';
+import { Field, reduxForm, formValueSelector } from 'redux-form/immutable';
+import { withStyles } from 'material-ui/styles';
 
 import messages from './messages';
 
-function InviteForm() {
+InviteForm.propTypes = {
+  submitting: bool,
+  classes: object.isRequired,
+  onSubmit: func.isRequired,
+  handleSubmit: func.isRequired,
+};
+
+const styles = () => ({
+  wrapper: {
+    position: 'relative',
+  },
+  progress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -13,
+    marginLeft: -13,
+  },
+});
+
+function InviteForm({ submitting, classes, onSubmit, handleSubmit, ...rest }) {
   return (
-    <Dialog open aria-labelledby="form-dialog-title">
+    <Dialog {...rest} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">
         <FormattedMessage {...messages.title} />
       </DialogTitle>
@@ -23,25 +48,39 @@ function InviteForm() {
         <DialogContentText>
           <FormattedMessage {...messages.description} />
         </DialogContentText>
-        <TextField
+        <Field
+          name="email"
+          component={TextField}
           autoFocus
-          margin="dense"
-          id="name"
           label={<FormattedMessage {...messages.emailFieldLabel} />}
           type="email"
           fullWidth
+          disabled={submitting}
+          validate={[isRequired, isEmail]}
         />
       </DialogContent>
       <DialogActions>
-        <Button color="primary">
+        <Button color="primary" disabled={submitting}>
           <FormattedMessage {...messages.cancelAction} />
         </Button>
-        <Button color="primary">
-          <FormattedMessage {...messages.inviteAction} />
-        </Button>
+        <div className={classes.wrapper}>
+          <Button color="primary" disabled onClick={handleSubmit(onSubmit)}>
+            <FormattedMessage {...messages.inviteAction} />
+          </Button>
+          {true && <CircularProgress size={26} className={classes.progress} />}
+        </div>
       </DialogActions>
     </Dialog>
   );
 }
 
-export default compose(pure)(InviteForm);
+const valueSelector = formValueSelector('form/inviteUser');
+
+export default compose(
+  connect((state) => valueSelector(state, 'email')),
+  withStyles(styles),
+  reduxForm({
+    form: 'form/inviteUser',
+  }),
+  pure
+)(InviteForm);
--- stories/Admin/InviteFormStory.js
+++ stories/Admin/InviteFormStory.js
@@ -1,9 +1,16 @@
 import React from 'react';
-// import { boolean, select, text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import InviteForm from 'components/InviteForm';
 
 function InviteFormStory() {
-  return <InviteForm />;
+  return (
+    <InviteForm
+      open
+      onSubmit={action('onSubmit')}
+      error={text('Error', null)}
+    />
+  );
 }
 
 export default InviteFormStory;
